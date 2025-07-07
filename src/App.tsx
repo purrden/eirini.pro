192c881267563a2dd38667ba026aa332ea2d7250
denzenin <denis.zenin@gmail.com>
2025-07-07 20:25:04 +0100

    header and intro sections ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,89 @@
 import "./App.css";
-const indexes = Array(4).fill(0);
-// import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+import Logo from "./assets/images/logo.svg";
+import EiriniUserpic from "./assets/images/eirini-userpic.svg";
+import HouseAmongLeaves from "./assets/lottie/house_among_leaves.json";
+import BeamingSticks from "./assets/lottie/beaming_sticks.json";
+import RecyclingSign from "./assets/lottie/recycling_sign.json";
+import CornerSticks from "./assets/lottie/corner_sticks.json";
+import DancingCircle from "./assets/lottie/dancing_circles.json";
+import PeoplePlantingTogether from "./assets/lottie/people_planting_together.json";
+import Stones from "./assets/lottie/stones.json";
+import Zigzags from "./assets/lottie/zigzags.json";
 import { Player } from "@lottiefiles/react-lottie-player";
 
-const animation = {
-  houseAmongLeavesAnimation: {
-    path: "./lottie/house_among_leaves.json",
-  },
-  peoplePlantingTogether: {
-    path: "./lottie/people_planting_together.json",
-  },
-};
+import Content from "./content/main.json";
+
+function getPiecesOf(text: string) {
+  // We expect only one nbsp object between a pair of * symbols
+  // example:
+  // "It will cost *7 pounds* and change"
+  //   => ["It will cost ", "7 pounds", " and change"]
+  const nbspContentPattern = /^(.*?)\*(.*?)\*(.*)$/;
+  return (nbspContentPattern.exec(text) || []).filter((s) => s !== text);
+}
+
+interface ChapterProps {
+  text: string;
+}
+
+function Chapter({ text }: ChapterProps) {
+  const pieces = getPiecesOf(text);
+
+  if (pieces.length < 2) {
+    return <p className="intro-chapter">{text}</p>;
+  }
+
+  const nbspParts = pieces[1].split(" ");
+  return (
+    // TODO: refactor all of it into something that returns nbsp'ed
+    <p className="intro-chapter">
+      {pieces[0]}
+      {`${nbspParts[0]}`}&nbsp;{`${nbspParts[1]}`}
+      {pieces[2]}
+    </p>
+  );
+}
+
+const introAnimations = [
+  PeoplePlantingTogether,
+  RecyclingSign,
+  HouseAmongLeaves,
+];
 
 function App() {
   return (
-    <div
-      className="wrapper"
-      style={
-        {
-          // backgroundColor: "#b56767",
-          // position: "absolute",
-          // width: "100%",
-          // height: "100%",
-        }
-      }
-    >
-      {/* <svg>
-        <filter id="noise" x="0%" y="0%" width="100%" height="100%">
-          <feTurbulence baseFrequency={0.39} />
-          <feColorMatrix
-            type="matrix"
-            values="0.3 0 0 0 0
-                    0 0 0 0 0
-                    0 0 0 0 0
-                    0 0 0 0.4 0"
-          />
-        </filter>
+    <div>
+      <header>
+        <div className="centered-column">
+          <div className="row">
+            <img id="logo" src={Logo} />
+            <button id="lets-have-chat-button">
+              <img id="userpic" src={EiriniUserpic} />
+              <div id="lets-chat-bubble">
+                <p className="desktop">Let's have a chat</p>
+                <p className="mobile">Let's chat</p>
+              </div>
+            </button>
+          </div>
+        </div>
+      </header>
 
-        <rect x="0" y="0" width="100%" height="100%" filter="url(#noise)" />
-      </svg>
-
-      <div>
-        {indexes.map((_, i) => {
-          console.log("pean");
-          return (
-            <div key={`lottie-a-${i + 1}`}>
-              <h1>ANIMA: {i + 1}</h1>(
-              <Player
-                style={{ width: 100, height: 100 }}
-                src={`./lottie/a${i + 1}.json`}
-                loop
-                autoplay
-              ></Player>
-              );
+      <main>
+        <section id="eirini-intro">
+          <div className="centered-column">
+            <div className="cards row items-aligned-top">
+              {introAnimations.map((animation, index) => (
+                <div className={`intro-card intro-card-${index}`} key={`intro-card-${index}`}>
+                  <div className="player-container">
+                    <Player autoplay loop src={animation} />
+                  </div>
+                  <Chapter text={Content.page.section.intro[index]} />
+                </div>
+              ))}
             </div>
-          );
-        })} */}
-      {/* </div> */}
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
